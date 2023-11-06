c51025329302caf6e572fe963436805ac523f9dc
andres esteban ruiz <140468504+andresEstebanRuiz@users.noreply.github.comse>
2023-11-05 20:07:32 -0500

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
       </form>
       {
         isLoading
-        ?<h2>loading...</h2>
+        ? <div className='app__loading'><h2 className='app__loader'></h2></div>
         :(
         hasError || locationId ==='0'
         ? <h2>❌ Hey! you must provide an id from 1 to 126❌</h2>
